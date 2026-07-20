fa610a6b466cf47711ccdf32836db39aecdff6d7
Shole Alirezaei <alirezaei.shole@gmail.com>
2026-07-20 16:31:39 +0300

    feat(home): create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='flex flex-col gap-y-10'>
+      <section>
+        <div className='flex flex-col items-center space-y-4 pt-12 text-center'>
+          <Image
+            src='/images/avatar.png'
+            alt='avatar'
+            width='260'
+            height='260'
+            className='rounded-full overflow-hidden'
+          />
+          <h1 className='text-4xl font-bold'>Sholeh Alirezaei</h1>
+          <p className='text-xl text-base-content/80'>
+            Senior Full Stack Developer
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
