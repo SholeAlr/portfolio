f5521663022b4cd53e5b0ff77def85d41ccc7ff8
Shole Alirezaei <alirezaei.shole@gmail.com>
2026-07-23 18:32:27 +0300

    feat(button): remove unnecessary hover effects

--- components/ui/Button/constants/index.ts
+++ components/ui/Button/constants/index.ts
@@ -2,19 +2,15 @@ export const variants = {
   primary: `
     bg-pink-600
     text-white
-    hover:bg-pink-500
     hover:shadow-[0_0_30px_rgba(219,39,119,0.45)]
   `,
 
   secondary: `
     border
     border-pink-600
-    bg-zinc-900/40
+    bg-none
     text-pink-600
-
-    hover:bg-pink-500
-    hover:text-white
-    hover:shadow-[0_0_20px_rgba(219,39,119,0.25)]
+    hover:shadow-[0_0_30px_rgba(219,39,119,0.45)]
   `,
 
   ghost: `
