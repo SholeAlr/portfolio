bd53e0d18d8ec6ab2c14f992a9a8c904aaed1965
Shole Alirezaei <alirezaei.shole@gmail.com>
2026-09-04 00:50:49 +0300

    feat: create remaining pages

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <p className='text-7xl text-pink-600'>Coming Soon!</p>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/services/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const page = () => {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+      <p className='text-7xl text-pink-600'>Coming Soon!</p>
+    </div>
+  );
+};
+
+export default page;
